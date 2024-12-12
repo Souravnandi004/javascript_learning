6688fb62e802959f5b64cd4b46dcae9b4bed3b4e
Sourav Nandi <sunnynandi87@gmail.com>
2024-12-12 19:50:22 +0000

    add varibale

---
+++ 01_basics/variable.js
@@ -0,0 +1,19 @@
+const accountId = 1324325
+let accountEmail = "sourav@gmail.com"
+var accountPassword = "2444366"
+accountCity = "Chennai"
+let accountBusiness;  //undefined
+
+
+// accountId = 722122 not allowed
+
+accountEmail = "sunny@gmail.com"
+var accountPassword = "44445555"
+accountCity = "Bishnupur"
+
+/*
+prefer not to use var
+because in block scope and functional scope
+*/
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
